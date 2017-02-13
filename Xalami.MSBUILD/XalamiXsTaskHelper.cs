c1ea6a129cfb2c85f3c0dd4f6460da929f5ad4f3
Neil McAlister <neil.mcalister@futurice.com>
2017-02-13 16:35:43 +0200

    Remove debug flag

--- Xalami.MSBUILD/XalamiXsTaskHelper.cs
+++ Xalami.MSBUILD/XalamiXsTaskHelper.cs
@@ -103,9 +103,7 @@ namespace Xalami.MSBUILD
         }        
         
         private static string GenerateFilesNode(string csprojXml, string projectTemplateFolderName)
-        {
-            System.Diagnostics.Debugger.Launch();
-
+        {            
             //Get project items
             List<CsprojItem> files = new List<CsprojItem>();
             XDocument xdoc;
