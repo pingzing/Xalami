7152251754b2b3df7e8f198a0cdc4c62a36ea5eb
Neil McAlister <nmca@fl679.local>
2017-02-17 11:52:11 +0200

    Make TemplateGenerator mono-friendly

--- Xalami.TemplateGenerator/VsGroupTemplateTask.cs
+++ Xalami.TemplateGenerator/VsGroupTemplateTask.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Build.Framework;
-using System;
+﻿using System;
 using System.IO;
 using System.IO.Compression;
 using System.Reflection;
--- Xalami.TemplateGenerator/XalamiVsTaskBase.cs
+++ Xalami.TemplateGenerator/XalamiVsTaskBase.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 using System.IO;
 using System.Xml.Linq;
 using System.Collections;
